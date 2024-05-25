2a4598395ebad4f51afc91833a6121725aa728c0
Amine Fadssi <amin.fadssi@gmail.com>
2024-05-25 16:59:59 +0100

    first commit

---
+++ src/main/java/com/example/ebanking/repositories/BankAccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.ebanking.repositories;
+
+import org.sid.ebankingbackend.entities.BankAccount;
+import org.sid.ebankingbackend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+}
